79cfaeb87496c435179ccce7656bf185af58e47d
AdrianSanchez1510 <114549612+AdrianSanchez1510@users.noreply.github.com>
2024-06-13 15:10:55 -0500

    Creacion de formulario (Inicio)

--- app/(tabs)/profesor/assignments.js
+++ app/(tabs)/profesor/assignments.js
@@ -1,24 +1,100 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
+import React, {useState} from 'react';
+import {Alert, Modal, StyleSheet, Text, Pressable, View} from 'react-native';
 
 const ProfesorAssignments = () => {
+  
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Profesor Asignaciones</Text>
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Creacion de Asignacion</Text>
+
+            <Text style={styles.modalText}>Grupo</Text>
+
+            <Text style={styles.modalText}>Nombre de la asignacion</Text>
+
+            <Text style={styles.modalText}>Descripcion</Text>
+
+            <Text style={styles.modalText}>Fecha de Entrega</Text>
+
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Cancelar</Text>
+            </Pressable>
+
+            <Pressable
+              style={[styles.button, styles.buttonConfirm]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Confirmar</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}>
+        <Text style={styles.textStyle}>Crear Asignacion</Text>
+      </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: 'black',
+  },
+  buttonClose: {
+    backgroundColor: 'red',
+  },
+  buttonConfirm: {
+    backgroundColor: 'green',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
 
 export default ProfesorAssignments;
